8f4f9f8a7ceefcd3483f67afe1f9c0d7ffb087c0
TheSmartnik <misharinn@gmail.com>
2023-09-20 03:08:39 +0400

    Add cancel script

---
+++ multi-nft-lootbox/scripts/cancelLootbox.ts
@@ -0,0 +1,32 @@
+import { toNano, Address } from 'ton-core';
+import { Lootbox } from '../wrappers/Lootbox';
+import { NetworkProvider, sleep } from '@ton-community/blueprint';
+
+export async function run(provider: NetworkProvider, args: string[]) {
+    const ui = provider.ui();
+
+    const lootboxAddress = Address.parse(args.length > 0 ? args[0] : await ui.input('Provide lootbox address:'));
+
+    if (!(await provider.isContractDeployed(lootboxAddress))) {
+        ui.write(`Error: Contract at address ${lootboxAddress} is not deployed!`);
+        ui.write('Run  npx blueprint run to deploy contract');
+        return;
+    }
+
+    const lootbox = provider.open(Lootbox.createFromAddress(lootboxAddress));
+
+    await lootbox.sendCancel(provider.sender(), toNano('0.05'));
+
+    ui.write('Waiting for lootbox to be canceled');
+
+    let attempt = 1;
+    while ((await lootbox.getChancesData()).chancesLength > 0) {
+
+        ui.setActionPrompt(`Attempt ${attempt}`);
+        await sleep(4000);
+        attempt++;
+    }
+
+    ui.clearActionPrompt();
+    ui.write(`Lootbox was successfully canceled`);
+}
